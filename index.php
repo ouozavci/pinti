cd2de3cfc4eed8c2ae7d1b64d09c744149c249e0
kaan taş <kaan ta>
2016-10-08 19:04:05 +0300

    login-logout işlemleri

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
+    session_start();
+
+    if(isset($_SESSION['id'])){
+        $_SESSION['isLogged']=true;
+        $userid=$_SESSION['id'];
+
+        require_once __DIR__.'/db/db_connect.php';
+        $db=new DB_CONNECT();
+        $result=mysql_query("SELECT * FROM users where id='$userid'");
+
+        if(!empty($result)){
+            if(mysql_num_rows($result)>0){
+                $result=mysql_fetch_array($result);
+
+                $username=$result["username"];
+
+                $_SESSION["username"]=$username;
+
+            }
+        }
+
+    }
+    else
+        $_SESSION["isLogged"]=false;
+
 ?>
 <html>
 <head>
@@ -7,13 +32,30 @@
 </head>
 <body>
 <div id="header">
-    <h1 id="logo">Pinti.com</h1>
-    <ul>
-        <li><a href="signin.php">Kayıt ol</a></li>
-        <li><a href="login.php">Giriş yap</a></li>
-        <li><a href="">Ürün Sat</a></li>
-        <li><a href="help.php">Yardım</a></li>
-    </ul>
+    <?php
+
+        if(!$_SESSION["isLogged"]) {
+            echo "
+            <h1 id = 'logo' > Pinti.com</h1 >
+            <ul >
+                <li ><a href = 'signin.php' > Kayıt ol </a ></li >
+                <li ><a href = 'login.php' > Giriş yap </a ></li >
+                <li ><a href = '' > Ürün Sat </a ></li >
+                <li ><a href = 'help.php' > Yardım</a ></li >
+            </ul >" ;
+        }
+        else{
+            echo "
+            <h1 id = \'logo\' > Pinti.com</h1 >
+            <ul >
+                <li>Hoşgeldin $username.</li>
+                <li ><a href = 'logout.php' > Çıkış yap </a ></li >
+                <li ><a href = '' > Ürün Sat </a ></li >
+                <li ><a href = 'help.php' > Yardım</a ></li >
+            </ul >
+            " ;
+        }
+    ?>
 </div>
 <h1>pinti.com</h1>
 <p>pinti.com en ucuz fiyatlarla yakında hizmetinizde.</p>
