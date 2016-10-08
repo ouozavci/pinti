698b61dac55f5b29f526033a2cfc3c23b62fcc32
ouozavci <ouozavci@gmail.com>
2016-10-08 11:57:16 +0300

    ilk commit. Proje henüz boş

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+?>
+<html>
+<head>
+<title>Main Page</title>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+<body>
+	<h1>pinti.com</h1>
+	<p>pinti.com en ucuz fiyatlarla yakında hizmetinizde</p>
+</body>
+</html>
