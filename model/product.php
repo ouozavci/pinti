a0b1889a3ad9e2f8512c7fd86d1fe6e5b6551935
Can <canbay9942@gmail.com>
2016-10-14 19:42:06 +0300

    product modeli oluşturuldu

---
+++ model/product.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Can Canbay
+ * Date: 13.10.2016
+ * Time: 23:14
+ */
+require_once __DIR__.'/../model/Product.php';
+require_once __DIR__.'/db/db_connect.php';
+class Product
+{
+
+    public $id;
+    public $name;
+    public $quantity;
+    public $price;
+
+    public static function getAll(){
+        $db = new DB_CONNECT();
+        $mysqli = $db->connect();
+        $result = mysqli_query($mysqli, "SELECT * FROM products");
+        return $result;
+    }
+    
+}
