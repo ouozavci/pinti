870392a85b76c6e61407fdf9cbe20fb7cdd15b9e
kaan taş <kaan ta>
2016-10-16 21:43:19 +0300

    geçici bootstrap menubar eklendi

---
+++ js/menubar.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Casper on 16.10.2016.
+ */
+$(document).ready(function(){
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');
+        }
+    );
+});
