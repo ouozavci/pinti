5b97e950a5be7a59d6e01343365351e9ea13e6d9
ouozavci <ouozavci@gmail.com>
2016-10-31 21:56:48 +0300

    Product.php modeline methodlar eklendi

--- model/Product.php
+++ model/Product.php
@@ -12,15 +12,30 @@ class Product
 
     public $id;
     public $name;
-    public $quantity;
     public $price;
+    public $category_id;
+    public $brand_id;
+    public $seller_id;
+    public $image_url;
 
     public static function getAll(){
         $db = new DB_CONNECT();
         $mysqli = $db->connect();
         $result = mysqli_query($mysqli, "SELECT * FROM products");
         return $result;
-
     }
-    
+
+    public static function getProductById($id){
+        $db = new DB_CONNECT();
+        $mysqli = $db->connect();
+        $result = mysqli_query($mysqli,"SELECT * FROM products where id='$id'");
+        return $result;
+    }
+
+    public static function getProductByCategoryId($category_id){
+        $db = new DB_CONNECT();
+        $mysqli = $db->connect();
+        $result = mysqli_query($mysqli,"SELECT * FROM products where category_id='$category_id'");
+        return $result;
+    }
 }
