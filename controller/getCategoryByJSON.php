a93bc8dbdfefe15bb72f8a100c22af52889c1815
Can <canbay9942@gmail.com>
2016-10-31 22:24:47 +0300

    getCategoryByJSON webservice yarım kaldı

---
+++ controller/getCategoryByJSON.php
@@ -0,0 +1,25 @@
+<?
+require_once "../model/Category.php";
+if($_GET){
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $submenus = Category::getCategoryByParentId($id);
+        $categories = array();
+        $count=0;
+        while($submenu = mysqli_fetch_array ($submenus))
+        {
+            $category = new Category();
+            $category->id = $submenu['id'];
+            $category->name = $submenu['name'];
+            $category->parent_id = $submenu['parent_id'];
+            $categories[$count] = $category;
+            $count++;
+        }
+        $result = json_encode($categories);
+        echo $result;
+    }
+}
+
+
+
+?>
