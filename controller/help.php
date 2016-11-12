fdf40da206c8b60da87c9605a91a6fcafb61bcab
kaantas <kaantas@anadolu.edu.tr>
2016-11-13 00:19:23 +0300

    Help Sayfası yapıldı

--- controller/help.php
+++ controller/help.php
@@ -1,8 +1,3 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: oguz
- * Date: 13.10.2016
- * Time: 13:27
- */
+
 require_once __DIR__.'/../view/help.phtml';
--- controller/index.php
+++ controller/index.php
@@ -1,3 +1,3 @@
 <?php
-require_once "/../view/index.phtml";
+require_once "../view/index.phtml";
 ?>
