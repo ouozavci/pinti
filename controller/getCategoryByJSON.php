24d7cf83e4f71dc5b62ef7de2e3fa25a95e5b012
ouozavci <ouozavci@gmail.com>
2016-11-19 19:09:10 +0300

    ürün sat category optimizasyon ve devam butonu

--- controller/getCategoryByJSON.php
+++ controller/getCategoryByJSON.php
@@ -6,17 +6,19 @@ if($_POST){
         $submenus = Category::getCategoryByParentId($id);
         $categories = array();
         $count=0;
-        while($submenu = mysqli_fetch_array ($submenus))
-        {
-            $category = new Category();
-            $category->id = $submenu['id'];
-            $category->name = $submenu['name'];
-            $category->parent_id = $submenu['parent_id'];
-            $categories[$count] = $category;
-            $count++;
+        if($submenus) {
+            while ($submenu = mysqli_fetch_array($submenus)) {
+                $category = new Category();
+                $category->id = $submenu['id'];
+                $category->name = $submenu['name'];
+                $category->parent_id = $submenu['parent_id'];
+                $categories[$count] = $category;
+                $count++;
+            }
+            $result = json_encode($categories);
+            echo $result;
         }
-        $result = json_encode($categories);
-        echo $result;
+        else echo "no such category";
     }
 }
 
