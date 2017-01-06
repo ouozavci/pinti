5a7008540e7c33819ec100c79cb96ea3f1d4f314
cancanbay <canbay9942@gmail.com>
2017-01-06 19:25:37 +0300

    kategori ve ürün adına göre arama işlemleri gerçekleştirildi

--- model/Product.php
+++ model/Product.php
@@ -64,7 +64,7 @@ class Product
     public static function getProductsBySearchKeyword($keyword){
         $db = new DB_CONNECT();
         $mysqli = $db->connect();
-        $result = mysqli_query($mysqli,"SELECT * FROM products WHERE name LIKE '%{$keyword}%'");
+        $result = mysqli_query($mysqli,"SELECT * FROM products P INNER JOIN category C ON P.category_id = C.id WHERE P.name LIKE '%{$keyword}%' OR C.name LIKE '%{$keyword}%'");
         return $result;
     }
 }
